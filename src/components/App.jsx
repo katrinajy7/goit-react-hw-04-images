870d757a6d9b9366765b547fb0409ed027032a55
Katrina Joy Manon-og <145580105+katrinajy7@users.noreply.github.com>
2024-07-14 15:22:43 +0800

    testing pa more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,24 +18,9 @@
 
 
 import React, { useState, useEffect, useCallback } from 'react';
-import { fetchImages } from './servises/Api'; 
+import { fetchImages } from './servises/Api';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Loader } from './Loader/Loader';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
-
-const App = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadMore, setLoadMore] = useState(false);
-  const [error, setError] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [largeImageURL, setLargeImageURL] = useState('');
-
- useEffect(() => {
-    if (!searchQuery) return;
-    getImages(searchQuery, page);
-  }, [searchQuery, page]); 
