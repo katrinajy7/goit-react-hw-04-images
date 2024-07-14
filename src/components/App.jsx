a03f1be571e3e8925ad755ff489f771f72a4536b
Katrina Joy Manon-og <145580105+katrinajy7@users.noreply.github.com>
2024-07-14 16:09:37 +0800

    images done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,3 @@
-// import React, { useState, useEffect, userCallback } from 'react';
-// import { fetchImages } from './servises/Api';
-// import { Searchbar } from './Searchbar/Searchbar';
-// import { Loader } from './Loader/Loader';
-// import { ImageGallery } from './ImageGallery/ImageGallery';
-// import { Button } from './Button/Button';
-// import { Modal } from './Modal/Modal';
-
-
-
 
 
 import React, { useState, useEffect, useCallback } from 'react';
