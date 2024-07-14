c5c3e23ed06a9115eaf8d2655360a44d568bdd85
Katrina Joy Manon-og <145580105+katrinajy7@users.noreply.github.com>
2024-07-14 15:16:47 +0800

    unoy problema

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,16 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { fetchImages } from './servises/Api'; 
-import { Searchbar } from './Searchbar/Searchbar';
-import { Loader } from './Loader/Loader';
-import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Button } from './Button/Button';
-import { Modal } from './Modal/Modal';
-
-
-const App = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadMore, setLoadMore] = useState(false);
-  const [error, setError] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [largeImageURL, setLargeImageURL] = useState('');
-
-  useEffect(() => {
-    if (!searchQuery) return;
-    getImages(searchQuery, page);
-  }, [searchQuery, page]);
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework ay unoy problema gayud
+    </div>
+  );
+};
